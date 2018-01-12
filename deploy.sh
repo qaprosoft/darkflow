0db3817f9eaed35bc2fbb4e8e612961dad61eb72
vdelendik <vadim.delendik@gmail.com>
2018-01-12 20:56:35 +0000

    fix deployment steps

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,13 @@
 
 #set of shell commands to build darkflow and sync with cfg/ckpt etc data
 
+export DARKFLOW_HOME=/darkflow/darkflow
+export ALICE_HOME=/darkflow/alice
+
 echo DARKFLOW_HOME $DARKFLOW_HOME
 echo ALICE_HOME: $ALICE_HOME
 
+cd $DARKFLOW_HOME
 # build the Cython extensions in place.
 python3 setup.py build_ext --inplace
 
